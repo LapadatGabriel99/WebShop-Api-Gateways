7250f71d7ef322ed72ef3446db399b92e8092b11
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-07-18 15:54:12 +0300

    - Added WebShop.Web.Api.Gateway asp.net core empty web project.

---
+++ src/Gateways/Web/WebShop.Web.Api.Gateway/Program.cs
@@ -0,0 +1,15 @@
+namespace WebShop.Web.Api.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
